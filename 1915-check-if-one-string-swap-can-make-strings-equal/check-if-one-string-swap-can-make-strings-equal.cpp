4843f9ae12e56b74dcc690603be7cf20030d8968
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-05-01 03:02:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.25%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         if(s1 == s2) return true;
-        int cnt = 0;
         vector<int> indices;
         for(int i = 0 ; i < s1.size(); i++){
             if(s1[i] != s2[i]){
-                cnt++;
                 indices.push_back(i);
             } 
-            if(cnt > 2) return false;
+            if(indices.size() > 2) return false;
         }
-        if(cnt != 2) return false;
-        if(s1[indices[0]] == s2[indices[1]] && s1[indices[1]] == s2[indices[0]]) return true;
+        if(indices.size() != 2) return false;
+        if(s1[indices[0]] == s2[indices[1]] 
+        && s1[indices[1]] == s2[indices[0]]) return true;
         return false;
     }
 };
