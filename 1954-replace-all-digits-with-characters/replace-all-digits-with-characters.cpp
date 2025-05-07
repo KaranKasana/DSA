f57c74642ac43c1a79cc65b852b3db96763b1f79
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-05-07 18:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (51.66%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -3,7 +3,7 @@ public:
     string replaceDigits(string s) {
         for(int i = 1; i < s.size(); i++){
             if(isdigit(s[i])){
-                s[i] = char(s[i-1] + s[i] - '0');
+                s[i] = s[i-1] + s[i] - '0';
             }
         }
         return s;
