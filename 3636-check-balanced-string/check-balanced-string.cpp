88ecbf3dd7e71e7abaec00842c86265f4a4541d5
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-05-17 15:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.86%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.cpp
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     bool isBalanced(string num) {
-        int n = num.size(); 
         int sumEven = 0, sumOdd = 0;
-        int i = 0, j = 1;
-        while(i < n || j < n){
-            if(i < n){
-                sumEven += num[i] - '1' + 1;
-                i += 2;
-            }
-            if(j < n){
-                sumOdd += num[j] - '1' + 1;
-                j += 2;
-            }
+        for(int i = 0; i < num.size(); i++){
+            if(i % 2 == 0) sumEven += num[i] - '0';
+            else sumOdd += num[i] - '0';
         }
         return sumEven == sumOdd;
     }
