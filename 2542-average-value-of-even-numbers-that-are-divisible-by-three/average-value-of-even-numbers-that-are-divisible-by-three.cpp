674f43fa8d7f781b40668ef6cbf576f5169223fe
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-04 15:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (21.31%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.cpp
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.cpp
@@ -3,7 +3,7 @@ public:
     int averageValue(vector<int>& nums) {
         int sum = 0, cnt = 0;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] % 2 == 0 && nums[i] % 3 == 0){
+            if(nums[i] % 6 == 0){
                 sum += nums[i];
                 cnt++;
             }
