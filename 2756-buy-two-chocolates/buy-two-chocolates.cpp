26a613b7aae2f431d435af7f12ebc3e533c1f2e0
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-07 17:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (62.46%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -3,11 +3,11 @@ public:
     int buyChoco(vector<int>& prices, int money) {
         int first = INT_MAX, second = INT_MAX; 
         for(int i = 0; i < prices.size(); i++){
-            if(prices[i] <= first){
+            if(prices[i] < first){
                 second = first;
                 first = prices[i];
             }
-            else if(prices[i] >= first &&  prices[i] <= second){
+            else if(prices[i] < second){
                 second = prices[i];
             }
         }
