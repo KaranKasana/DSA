c8dcef0d9604035217ea1ac39491116d4f0ae016
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-10 18:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (8.71%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> ans;
+        int l = 0, r = s.length();
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == 'I') ans.push_back(l++);
+            else ans.push_back(r--);
+        }
+        ans.push_back(l);
+        return ans;
+    }
+};
