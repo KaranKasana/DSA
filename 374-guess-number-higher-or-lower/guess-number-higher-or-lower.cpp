cd353bf1d001d75aa65445569b9b2e67ab01a0aa
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-20 17:22:54 +0530

    Time: 2 ms (54.74%) | Memory: 8 MB (39.40%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -15,7 +15,7 @@ public:
             int mid = l + (r - l) / 2;
             if(guess(mid) == 0) return mid;
             else if(guess(mid) == 1) l = mid + 1;
-            else if(guess(mid) == -1) r = mid - 1;
+            else r = mid - 1;
         }
         return -1;
     }
