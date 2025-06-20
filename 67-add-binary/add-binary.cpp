2332a1b1f50041bb7f7de81dd1cd0e49aa46df41
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-20 19:38:47 +0530

    Time: 3 ms (25.73%) | Memory: 11.2 MB (17.78%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -6,14 +6,8 @@ public:
         string ans = "";
         while(i >= 0 || j >= 0 || carry){
             int sum = carry;
-            if(i >= 0){
-                sum += a[i] - '0';
-                i--;
-            }
-            if(j >= 0){
-                sum += b[j] - '0';
-                j--;
-            }
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
             ans = char(sum % 2 + '0') + ans;
             carry = sum / 2;
         }
