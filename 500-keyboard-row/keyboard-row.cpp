20e1bc237077240e3c64dc3ade96c2d67c05c9f2
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-25 13:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (99.15%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        string first = "qwertyuiop", second = "asdfghjkl", third = "zxcvbnm";
+        int a[26];
+        for(int i = 0; i < first.length(); i++) a[first[i] - 'a'] = 1;
+        for(int i = 0; i < second.length(); i++) a[second[i] - 'a'] = 2;
+        for(int i = 0; i < third.length(); i++) a[third[i] - 'a'] = 3;
+        vector<string> ans;
+        for(int i = 0; i < words.size(); i++){
+            bool flag = true;
+            for(int j = 1; j < words[i].length(); j++){
+                if(a[tolower(words[i][j]) - 'a'] != a[tolower(words[i][0]) - 'a']){
+                    flag = false;
+                } 
+            }
+            if(flag) ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
