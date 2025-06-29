67b63164576b0de150513c52ae62ae7043cbac8d
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-06-29 14:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (76.38%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size() > 1){
+            sort(stones.begin(), stones.end(), greater<int>());
+            if(stones[0] == stones[1]){
+                stones.erase(stones.begin());
+                stones.erase(stones.begin());
+            }
+            else{
+                stones[1] = stones[0] - stones[1];
+                stones.erase(stones.begin());
+            }
+        }
+        if(stones.empty()) return 0;
+        return stones[0];
+    }
+};
