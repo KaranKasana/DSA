791bd98a0e2464290002189d97694e0fb02f4fbb
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-02 15:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (93.23%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -3,16 +3,14 @@ public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int cnt = 0;
         for(int i = 0; i < fruits.size(); i++){
-            bool flag = false;
             for(int j = 0; j < baskets.size(); j++){
                 if(fruits[i] <= baskets[j]){
-                    flag = true;
                     baskets[j] = -1;
+                    cnt++;
                     break;
                 }
             }
-            if(!flag) cnt++;
         }
-        return cnt;
+        return fruits.size() - cnt;
     }
 };
