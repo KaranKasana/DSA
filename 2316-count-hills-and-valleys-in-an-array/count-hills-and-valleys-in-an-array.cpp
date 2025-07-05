4e1bc4102e0d0240ca8f2809607d840ba88f554c
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-05 11:24:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (18.15%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -2,21 +2,10 @@ class Solution {
 public:
     int countHillValley(vector<int>& nums) {
         int cnt = 0;
-        int i = 1;
-        while(i < nums.size() - 1){
-            if(nums[i] == nums[i-1]){
-                i++;
-                continue;
-            }
-            int left = i - 1;
-            int right = i + 1;
-            while(left >= 0 && nums[left] == nums[i]) left--;
-            while(right < nums.size() && nums[right] == nums[i]) right++;
-            if(left >= 0 && right < nums.size()){
-                if(nums[left] < nums[i] && nums[right] < nums[i]) cnt++;
-                else if(nums[left] > nums[i] && nums[right] > nums[i]) cnt++;
-            }
-            i++;
+        for(int i = 1; i < nums.size() - 1; i++){
+            if(nums[i] == nums[i+1]) nums[i] = nums[i-1];
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) cnt++;
+            if(nums[i] < nums[i-1] && nums[i] < nums[i+1]) cnt++;
         }
         return cnt;
     }
