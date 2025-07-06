53776dd41e2846844389d3835573b988bc6b5db1
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-06 19:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (48.24%) - LeetSync

--- 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int sum = 0, minimum = INT_MAX;
+        int sum = 0, minimumPrefix = 0;
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
-            minimum = min(minimum, sum);
+            minimumPrefix = min(minimumPrefix, sum);
         }
-        if(minimum < 0) return 1 - minimum;
+        if(minimumPrefix < 0) return 1 - minimumPrefix;
         return 1;
     }
 };
