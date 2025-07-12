6e1d3416f6c1f92877173fb6a2b4a6a48392ad2d
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-12 13:38:16 +0530

    Time: 3 ms (19.63%) | Memory: 16.4 MB (69.16%) - LeetSync

--- 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.cpp
+++ 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
-        int arr[1001] = {0};
+        unordered_map<int, int> mpp;
         for(int i = 1; i < nums.size(); i++){
             if(nums[i - 1] == key){
-                arr[nums[i]]++;
+                mpp[nums[i]]++;
             }
         }
         int maxi = 0, res = 0;
-        for(int i = 1; i <= 1000; i++){
-            if(arr[i] > maxi){
-                maxi = arr[i];
-                res = i;
+        for(auto it : mpp){
+            if(it.second > maxi){
+                maxi = it.second;
+                res = it.first;
             }
         }
         return res;
