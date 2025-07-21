02f2dff6fad48419e817a8dd58154dbbd9298e2e
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-21 17:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (61.03%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.cpp
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     bool hasSpecialSubstring(string s, int k) {
-        for(int i = 0; i <= s.length() - k; i++){
-            bool flag = true;
-            for(int j = i; j < i + k - 1; j++){
-                if(s[j] != s[j + 1]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(!flag) continue;
-            else{
-                if(i > 0){
-                    if(s[i] == s[i - 1]) flag = false;
-                }
-                if(i < s.length() - 1){
-                    if(s[i] == s[i + k]) flag = false;
-                }
-            }
-            if(flag) return true;
+        int n = s.length();
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(s[j] == s[i]) continue;
+            if(i - j == k) return true;
+            j = i;
         }
-        return false;
+        return (n - j) == k;
     }
 };
