1159ee78b843fdc5cf1036a799ae050750f53c09
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-07-29 18:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (76.48%) - LeetSync

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        vector<int> v(26, 0);
+        for(int i = 0; i < word1.length(); i++){
+            v[word1[i] - 'a']++;
+        }
+        for(int i = 0; i < word2.length(); i++){
+            v[word2[i] - 'a']--;
+        }  
+        for(int diff : v){
+            if(abs(diff) > 3) return false;
+        }
+        return true;
+    }
+};
