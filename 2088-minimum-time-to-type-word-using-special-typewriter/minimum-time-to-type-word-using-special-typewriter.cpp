1d23bab2eca5ac91cb2262b3f2f5ee3ee206d27f
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-09-05 14:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (70.93%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.cpp
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.cpp
@@ -3,10 +3,10 @@ public:
     int minTimeToType(string word) {
         int res = 0;
         res += min(abs('a' - word[0]),
-                abs(26 - (abs('a' - word[0]) % 26)));
+                abs(26 - abs('a' - word[0])));
         for(int i = 1; i < word.size(); i++){
             res += min(abs(word[i] - word[i - 1]),
-                abs(26 - (abs(word[i] - word[i - 1]) % 26)));
+                abs(26 - abs(word[i] - word[i - 1])));
         }
         return res + word.size();
     }
