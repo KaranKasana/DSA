89416bb630cafae059d217b54b7d7d1fa761807f
Karan Kasana <139806244+KaranKasana@users.noreply.github.com>
2025-09-05 14:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (93.60%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.cpp
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int minTimeToType(string word) {
         int res = 0;
-        res += min(abs('a' - word[0]) % 26,
-                abs(26 - (abs('a' - word[0]) % 26)));
+        res += min(abs('a' - word[0]), 26 - abs('a' - word[0]));
         for(int i = 1; i < word.size(); i++){
-            res += min(abs(word[i] - word[i - 1]) % 26,
-                abs(26 - (abs(word[i] - word[i - 1]) % 26)));
+            res += min(abs(word[i] - word[i - 1]), 26-abs(word[i] - word[i - 1]));
+   
         }
         return res + word.size();
     }
